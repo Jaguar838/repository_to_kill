646f5a0df38c10607fc342cce140c855b1f80eaa
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-06-16 13:58:46 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import 'normalize.css'
-import './sass/main.scss';
+import './styles.css';
+import './js/chart';
+import './js/currentCity.js';
+import './js/fiveDays.js';
+import './js/time';
+import './js/quotes.js'
+import './js/moreInfo.js'
+import './js/backgroundImg.js';
+import './js/findMe.js' ;
+import './js/favCity.js';
+import './js/main.js';
 
-import apiService from './js/api/apiService'
-
-const api = new apiService();
-console.log(api.makeRequest());
