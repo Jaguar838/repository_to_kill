49dd809484bc3b4f54c44f7c4543183bf05e39ce
Jaguar838 <oleg.yamgurov@gmail.com>
2021-06-17 22:39:14 +0300

    structura

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import 'normalize.css'
+import 'normalize.css';
 import './sass/main.scss';
-
-import apiService from './js/api/apiService'
-
-const api = new apiService();
-console.log(api.makeRequest());
+import './js/main';
--- src/js/api/apiService.js
+++ src/js/api/apiService.js
@@ -1,21 +1,33 @@
 import axios from 'axios'; // дока https://github.com/klesarev/axios-rus-docs/tree/master/docs
 
-export default class apiService {
-    constructor() {
+const API_KEY = 'e0f5a2b3f12c3f7ea9352edce7e33432';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
-        this.searchQuery = '';
-    }
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    // this.page = page;
+  }
+  //трендовые фильмы дня
+  async getTrendingMovies(pageNumber) {
+    const { data } = await axios.get(`/trending/movie/day?api_key=${API_KEY}&page=${pageNumber}`);
+    const { results, total_pages, page, total_results } = data;
+    return { results, total_pages, page, total_results };
+  }
 
-    async makeRequest() {
+  // поиск по ключевому слову
+  async getMovieByQuery(searchQuery, pageNumber) {
+    const { data } = await axios.get(
+      `/search/movie?api_key=${API_KEY}&page=${pageNumber}&query=${searchQuery}`,
+    );
+    const { results, total_pages, page, total_results } = data;
+    return { results, total_pages, page, total_results };
+  }
 
-        const config = {
-            method: 'get',
-            url: 'http://webcode.me'
-        }
+  //полная информация о фильме по ID
+  async getMovieById(id) {
+    const { data } = await axios.get(`/movie/${id}?api_key=${API_KEY}`);
 
-        let res = await axios(config)
-
-        console.log(res.status);
-    }
-
+    return data;
+  }
 }
